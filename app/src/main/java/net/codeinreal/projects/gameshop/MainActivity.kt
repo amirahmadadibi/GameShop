8ca9cedda353aa2972950fd39e5f3f6718bf3638
amirahmadadibi <amirahmadadibi@gmail.com>
2020-08-18 21:12:51 +0430

    adding every color and first layout and icon to the base project

---
+++ app/src/main/java/net/codeinreal/projects/gameshop/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.codeinreal.projects.gameshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
